fa6acaa2e04de001b289ca5bc4f09dbb06402a7b
piqriap <102671865+PikriaP@users.noreply.github.com>
2024-09-27 09:23:53 +0400

    1

---
+++ script.js
@@ -0,0 +1,12 @@
+const person= {
+    name: 'pikria',
+    age: 35,
+    height: 168,
+    profession: 'Jurnalist',
+    };
+
+for (const key in person){
+    console.log(`${key}: ${person[key]}`);
+    
+    
+}
