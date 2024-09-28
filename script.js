eab9edaef722e2fdf02f7bea78309b3ce104e53d
PikriaP <ffilishvili@gmail.com>
2024-09-28 07:50:04 +0400

    ex-3 check if num is perfect

--- script.js
+++ script.js
@@ -36,3 +36,31 @@ console.log(makeId(5));
 
 
 //3
+const numbersArray = [1, 24, 28, 25, 72, 6, 496, 12];
+
+ function isNumPerfect(num) {
+    let sum=0;
+   for (let i = 1; i < num; i++) {
+     if(num % i === 0){
+        sum+=i;
+     }
+    
+   } 
+   if (sum===num){
+    return true;
+   }else {
+    return false;
+   }
+ }
+
+
+ const checker = function numberCecker (arr){
+    arr.forEach(num=>{
+       if (isNumPerfect(num)) {
+        console.log(num + ' is perfect number');
+       }else{
+        console.log(num + " isn't perfect number");
+       }
+    });
+ }
+ checker(numbersArray);
